844ec84e21de698388a1867176c4823b912570fb
barbracunningham <barbra.cunningham.sc@gmail.com>
2017-03-23 17:55:43 +0000

    Update simple_concurrency.hs

--- simple_concurrency.hs
+++ simple_concurrency.hs
@@ -14,7 +14,7 @@ import qualified Haxl.Prelude as HP
 
 type Haxl a = GenHaxl () a --Create instance of the Haxl monad
 
---|| Create data source
+--Create data source
 data DeepThought a where
   Answer:: DeepThought Int
   deriving Typeable
@@ -26,23 +26,23 @@ instance Show1 DeepThought where
 instance Hashable (DeepThought a) where
     hashWithSalt salt Answer = hashWithSalt salt ()
   
-  --|| Create function to take data source and do stuff
+  --Create function to take data source and do stuff
   runDeepThought :: DeepThought a -> ResultVar a -> IO ()
   runDeepThought (Answer) var = putSuccess var 42 
   
-  --|| Tie data source to 'DataSourceName'
+  --Tie data source to 'DataSourceName'
   instance DataSourceName DeepThought where
     dataSourceName _ = "Deep Thought"
  
- --|| Tie data source to 'DataSource'
+ --Tie data source to 'DataSource'
  instance DataSource () DeepThought
     fetch _ _ _ reqs = SyncFetch $ HP.forM_ reqs $ \[BlockedFetch req var] -> runDeepThought req var
  
- --|| Tie data source to a 'StateKey'
+ --Tie data source to a 'StateKey'
  instance State DeepThought where
     data State DeepThought = NoState
  
- --|| Create an initial state 
+ --Create an initial state 
  initialState :: StateStore
  initialState = stateSet NoState stateEmpty
  
